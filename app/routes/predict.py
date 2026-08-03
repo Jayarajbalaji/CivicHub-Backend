6f9925e82e863df33b74288b1b2c296f6fd69c59
JAYARAJ B <jayaraj.bf1@gmail.com>
2026-08-03 20:57:57 +0530

    Fix category parameter for prediction

--- app/routes/predict.py
+++ app/routes/predict.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, UploadFile, File
+from fastapi import APIRouter, UploadFile, File, Form
 import os
 import shutil
 
@@ -9,10 +9,15 @@ router = APIRouter()
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
-@router.post("/predict")
-async def predict_issue(file: UploadFile = File(...)):
 
-    print("Received filename:", file.filename)
+@router.post("/predict")
+async def predict_issue(
+    file: UploadFile = File(...),
+    category: str = Form(...)
+):
+    print("===================================")
+    print("Prediction Request Received")
+    print("Category:", category)
 
     ext = os.path.splitext(file.filename)[1]
 
@@ -21,13 +26,15 @@ async def predict_issue(file: UploadFile = File(...)):
 
     filepath = os.path.join(UPLOAD_FOLDER, f"image{ext}")
 
-    print("Saving file to:", filepath)
-
     with open(filepath, "wb") as buffer:
         shutil.copyfileobj(file.file, buffer)
 
-    print("File exists:", os.path.exists(filepath))
+    print("Image Saved:", filepath)
 
-    result = predict(filepath)
+    print("Calling AI...")
+
+    result = predict(filepath, category)
+
+    print("AI Finished")
 
     return result
